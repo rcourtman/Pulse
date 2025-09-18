ac8ab7dc0cbcbc3beafcb3303609a74469d01f08
trokoloooo <134735866+trokoloooo@users.noreply.github.com>
2025-09-18 13:25:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ INSTALL_DIR="/opt/pulse"
 CONFIG_DIR="/etc/pulse"  # All config and data goes here for manual installs
 SERVICE_NAME="pulse"
 GITHUB_REPO="rcourtman/Pulse"
+BUILD_FROM_SOURCE=false
 
 # Wrapper for systemctl commands that might hang in unprivileged containers
 safe_systemctl() {
