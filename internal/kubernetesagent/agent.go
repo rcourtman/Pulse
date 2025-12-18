1e955255a627248a45975e34153e9b1b1216e1dc
rcourtman <courtmanr@gmail.com>
2025-12-18 16:42:51 +0000

    fix: remove unused sets import in kubernetesagent

--- internal/kubernetesagent/agent.go
+++ internal/kubernetesagent/agent.go
@@ -23,7 +23,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
