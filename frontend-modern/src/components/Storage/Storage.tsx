d3fe3b117deda8ba3d53f88ceda49ecf05914cd0
rcourtman <courtmanr@gmail.com>
2026-01-30 19:00:59 +0000

    feat(frontend): remove storage configuration panel

--- frontend-modern/src/components/Storage/Storage.tsx
+++ frontend-modern/src/components/Storage/Storage.tsx
@@ -8,7 +8,7 @@ import { StatusDot } from '@/components/shared/StatusDot';
 import { ComponentErrorBoundary } from '@/components/ErrorBoundary';
 import { UnifiedNodeSelector } from '@/components/shared/UnifiedNodeSelector';
 import { StorageFilter } from './StorageFilter';
-import { StorageConfigPanel } from './StorageConfigPanel';
+
 import { DiskList } from './DiskList';
 import { ZFSHealthMap } from './ZFSHealthMap';
 import { EnhancedStorageBar } from './EnhancedStorageBar';
@@ -625,9 +625,7 @@ const Storage: Component = () => {
           searchInputRef={(el) => (searchInputRef = el)}
           columnVisibility={columnVisibility}
         />
-        <Show when={connected()}>
-          <StorageConfigPanel nodeFilter={selectedNode()} searchTerm={searchTerm()} />
-        </Show>
+
       </Show>
 
       {/* Show simple search for disks */}
