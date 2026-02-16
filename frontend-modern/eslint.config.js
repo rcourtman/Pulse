cc82588b690bd5e9515b4e503587e9873527cd80
rcourtman <courtmanr@gmail.com>
2026-02-16 16:28:27 +0000

    chore(frontend): ignore generated api client in eslint

--- frontend-modern/eslint.config.js
+++ frontend-modern/eslint.config.js
@@ -5,7 +5,7 @@ import prettier from "eslint-config-prettier";
 import globals from "globals";
 
 export default tseslint.config(
-    { ignores: ["dist", "public", "node_modules"] },
+    { ignores: ["dist", "public", "node_modules", "src/api/generated"] },
     js.configs.recommended,
     ...tseslint.configs.recommended,
     {
